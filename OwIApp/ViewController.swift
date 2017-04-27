0ea039922c6e7f73030b5f72e54be75a440353d5
theji2bs <malidi.elhadad@gmail.com>
2017-04-27 09:53:56 +0200

    Initial Commit

---
+++ OwIApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OwIApp
+//
+//  Created by MALIDI ELHADAD on 27/04/2017.
+//  Copyright © 2017 MALIDI ELHADAD. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
